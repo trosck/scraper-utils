b8c064b0766cd11a418dc1f0f1e086189cda69c3
Sergey Belov <kort.dll@gmail.com>
2022-06-16 01:41:39 +0300

    add test for removeDoubleSpaces

--- test/clearHtml.test.js
+++ test/clearHtml.test.js
@@ -18,4 +18,3 @@ describe('clearHtml', () => {
     assert.equal(clearHtml(example), clearText)
   })
 })
-
---
+++ test/removeDoubleSpaces.test.js
@@ -0,0 +1,11 @@
+import assert from 'assert'
+import removeDoubleSpaces from '../core/removeDoubleSpaces.js'
+
+const example = '      Example Domain    This domain is for use in illustrative examples in documents. You may use this    domain in literature without prior coordination or asking for permission.    More information...  '
+const clearText = ' Example Domain This domain is for use in illustrative examples in documents. You may use this domain in literature without prior coordination or asking for permission. More information... '
+
+describe('removeDoubleSpaces', () => {
+  it('should remove double spaces', () => {
+    assert.equal(removeDoubleSpaces(example), clearText)
+  })
+})
