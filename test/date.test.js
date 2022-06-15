fb93edd50f4faf739678b2b758ed0bef5b1ba7f2
Sergey Belov <kort.dll@gmail.com>
2022-06-16 01:38:39 +0300

    add test for date

---
+++ test/date.test.js
@@ -0,0 +1,15 @@
+import assert from 'assert'
+import { parseRuDate } from '../core/date.js'
+
+describe('date', () => {
+  const testDate = (date, time, type = 'short') => it(
+    `should parse ${date}`,
+    () => assert.equal(
+      parseRuDate(date, type)?.getTime(), time
+    )
+  )
+
+  testDate('01 ноя 2020', 1604178000000)
+  testDate('28 Декабря 2016', 1482872400000, 'long')
+  testDate('23 октября 2019', 1571778000000, 'long')
+})
