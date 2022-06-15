56736698f8cbde60c254871546cf3fe8529f62fe
Sergey Belov <kort.dll@gmail.com>
2022-06-16 01:01:40 +0300

    docs: add comments

--- core/clearHtml.js
+++ core/clearHtml.js
@@ -4,7 +4,10 @@
  * @returns {string}
  */
 export default string => string
+  // tags
   ?.replace(/(<([^>]+)>)/g, '')
+  // codes
   ?.replace(/&[#\w\d]+;/g, ' ')
+  // line breaks
   ?.replace(/[\f\n\r\t\v\u00A0]/g, '')
   ?? null
