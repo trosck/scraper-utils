bf4867c2207c7736734142a529c19ed682b0fa8e
Sergey Belov <kort.dll@gmail.com>
2022-06-16 10:49:33 +0300

    fix: test for parseRuDate

--- test/date.test.ts
+++ test/date.test.ts
@@ -9,12 +9,12 @@ describe('date', () => {
   ) => it(
     `should parse ${date}`,
     () => assert.equal(
-      parseRuDate(date, type)?.getTime(),
+      parseRuDate(date)?.getTime(),
       new Date(time).getTime()
     )
   )
 
   testDate('01 ноя 2020', '2020/11/01')
-  testDate('28 Декабря 2016', '2016/12/28', 'long')
-  testDate('23 октября 2019', '2019/10/23', 'long')
+  testDate('28 Декабря 2016', '2016/12/28')
+  testDate('23 октября 2019', '2019/10/23')
 })
