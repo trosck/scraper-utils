3c83f5fd644623fa87105d91ec4efbd00c55a468
Sergey Belov <kort.dll@gmail.com>
2022-06-22 17:09:13 +0300

    fix: parseRuDate test

--- test/date.test.ts
+++ test/date.test.ts
@@ -3,17 +3,17 @@ import { parseRuDate } from '../core/date'
 
 describe('date', () => {
   const testDate = (
-    date: string,
+    ruDate: string,
     time: string
   ) => it(
-    `should parse ${date}`,
+    `should parse ${ruDate}`,
     () => assert.equal(
-      parseRuDate(date)?.getTime(),
+      parseRuDate(ruDate)?.getTime(),
       new Date(time).getTime()
     )
   )
 
-  testDate('01 ноя 2020', '2020/11/01')
-  testDate('28 Декабря 2016', '2016/12/28')
-  testDate('23 октября 2019', '2019/10/23')
+  testDate('01 ноя 2020', '2020-11-01T00:00:00.000Z')
+  testDate('28 Декабря 2016', '2016-12-28T00:00:00.000Z')
+  testDate('23 октября 2019', '2019-10-23T00:00:00.000Z')
 })
